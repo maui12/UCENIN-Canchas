eff971fd40beba2a253e0407d641c4a69746a7ff
maui12 <103455248+maui12@users.noreply.github.com>
2025-05-04 21:38:30 -0400

    Modelo de inicio de sesión básico
    inputs para nombre de usuario y contraseña
    botón de Iniciar sesión y Registrarse

--- canchas-ucenin/frontend/src/App.js
+++ canchas-ucenin/frontend/src/App.js
@@ -1,7 +1,16 @@
+import React from 'react';
+import './App.css';
+
 function App() {
   return (
-    <div>
-      <h1>Canchas UCENIN </h1>
+    <div className="container">
+      <h1>Canchas UCENIN</h1>
+      <div className="form">
+        <input type="text" placeholder="Nombre de usuario" />
+        <input type="password" placeholder="Contraseña" />
+        <button>Iniciar Sesión</button>
+        <p>¿No tienes cuenta? <a href="#">Registrarse</a></p>
+      </div>
     </div>
   );
 }
