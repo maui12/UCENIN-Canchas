cd990a24810ac57301b91e4f2bd70e0e156d4b1f
IenZAVALA-S <166187325+IenZAVALA-S@users.noreply.github.com>
2025-05-06 00:53:53 -0400

    Update Login.js

--- canchas-ucenin/frontend/src/pages/Login.js
+++ canchas-ucenin/frontend/src/pages/Login.js
@@ -14,7 +14,7 @@ function Login() {
     }
 
     try {
-      const response = await fetch('http://localhost:3001/api/login', {
+      const response = await fetch('http://localhost:3000/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
